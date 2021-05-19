31898c3aa9e81acfb41091e7059c2c8e2b2fd7ff
orazkarl <orazkarl>
2021-05-19 19:06:03 +0600

    requirements.txt

--- insta_parser/settings.py
+++ insta_parser/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-33vbukrmj*p@xg&zqmfwr&t$aec#hhd7&f$h7d)ffkj3r4j9u@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- mainapp/views.py
+++ mainapp/views.py
@@ -9,12 +9,12 @@ class HomeView(generic.TemplateView):
     """
     View для главной страницыг. Парсинг одного ника
     """
-    template_name = 'home.html' # html файл который мы видем когда зайдем на страницу
+    template_name = 'home.html'  # html файл который мы видем когда зайдем на страницу
 
     # post запрос чтобы парсит пользователя
     def post(self, request):
-        url = request.POST['profile_url'] # из запроса берем url
-        insta_user = get_user_info(url=url) # отправляем наш url в парсер и ответ получаем объект
+        url = request.POST['profile_url']  # из запроса берем url
+        insta_user = get_user_info(url=url)  # отправляем наш url в парсер и ответ получаем объект
         # После окончание запроса для клиента открываем user_info.html и отправляем в эту старницу наш объект чтобы клиент видел результат
         return render(request, template_name='user_info.html', context={'insta_user': insta_user})
 
@@ -23,20 +23,23 @@ class ParserMultipleUsernamesView(generic.TemplateView):
     """
         View для парсинг списка
     """
-    template_name = 'parsermultipleusernames.html' # html файл который мы видем когда зайдем на страницу
+    template_name = 'parsermultipleusernames.html'  # html файл который мы видем когда зайдем на страницу
 
     # post запрос чтобы парсит пользователей
     def post(self, request):
-        urls = request.POST['profiles_urls'] # из запроса берем список url-ов
-        urls = urls.splitlines() #
-        insta_users_usernames = []
-        for url in urls:
-            if url[:25] == 'https://www.instagram.com':
-                print(url)
-                insta_user = get_user_info(url=url)
-                insta_users_usernames.append(insta_user.username)
-        insta_users = InstagramUserProfile.objects.filter(username__in=insta_users_usernames)
+        urls = request.POST['profiles_urls']  # из запроса берем список url-ов
+        urls = urls.splitlines()  # наш список url-ов разделены по абзацу. Мы их переобразуем в list url-ов
+        insta_users_usernames = []  # создаем пустой list чтобы после парсинга туде добавить ник пользователей
+        for url in urls:  # создаем цикл, чтобы парсить каждый url отдельно
+            if url[:25] == 'https://www.instagram.com':  # проверка url, что он от инстаграма
+                insta_user = get_user_info(url=url)  # отправляем наш url в парсер и ответ получаем объект
+                insta_users_usernames.append(insta_user.username)  # добавляем ник пользователя в наш пустой список
+        # Чтобы показалть результат клиенту мы фильтруем наш модель(таблицу из бд), и возьмем только те объекты которые ник есть в нашем списке 'insta_users_usernames'
+        insta_users = InstagramUserProfile.objects.filter(
+            username__in=insta_users_usernames)  # insta_users это queryset. Queryset это список объектов заданной модели.
+        # Если в insta_users что то есть то для клиента открываем multipleusers_info.html и отправляем в эту старницу наш queryset чтобы клиент видел результат
         if insta_users:
             return render(request, template_name='multipleusers_info.html', context={'insta_users': insta_users})
+        # Если insta_users пустой то для клиента возвращаем страница с парсингом списков
         else:
             return redirect('parser_multiple_usernames')
